033dc36b84da733d3e6939c9b88e4d59f5963f78
Dear Comrade <86433628+ComradeDear@users.noreply.github.com>
2021-08-27 23:43:31 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -52,7 +52,7 @@ for name in files:
 
 import userbot._core
 
-print(f"""MAFIABOT IS ON!!! MAFIABOT VERSION :- {mafiaversion} YOUR 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @MafiaBot_Support .""")
+print(f"""GANGSTER-BOT IS ON!!!TYPE .ALIVE .""")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
